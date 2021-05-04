66a1eb17c6d6ee0462961551618b17174f1cdddf
jfabellera <jfabellera@gmail.com>
2021-05-04 18:28:42 -0500

    update version checking process and redirects updating process

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+set -e
+
+BASE_DIR=/home/rev/REV-Chromium-Builds
+BASE_GIT=github.com/REVrobotics/REV-Chromium-Builds.git
 CHROMIUM_DIR=/home/rev/chromium/src
 APK_OUTPUT_DIR=$CHROMIUM_DIR/out/Default/apks
 METADATA_DIR=/home/rev/Software-Update-Metadata
@@ -8,13 +12,22 @@ VERSION_URL="https://omahaproxy.appspot.com/all?os=android&channel=stable"
 VERSION_CSV=$(curl $VERSION_URL)
 LATEST_VERSION=$(echo $VERSION_CSV | awk -v FS=' ' '{print $2}' | awk -v FS=',' '{print $3}')
 
-cd $CHROMIUM_DIR
+cd $BASE_DIR
+
+git pull https://$GITHUB_TOKEN:x-oauth-basic@$BASE_GIT master
+git tag -l | xargs git tag -d
+git fetch https://$GITHUB_TOKEN:x-oauth-basic@$BASE_GIT master --tags
 
 # Exit if current tag matches latest stable version
-CURRENT_VERSION=$(git describe --tags)
-[[ $CURRENT_VERSION == $LATEST_VERSION ]] && exit 0
+CURRENT_VERSION=$(git describe --tags `git rev-list --tags --max-count=1`) |:
+if [[ $CURRENT_VERSION == $LATEST_VERSION ]]; then
+    echo "$CURRENT_VERSION is already the latest version"
+    exit 0
+fi
 
 echo "New release found: $LATEST_VERSION"
+
+cd $CHROMIUM_DIR
 
 # Get latest commits
 git rebase-update
@@ -65,21 +78,24 @@ WEBVIEW_URL=$(curl \
 # Update Software-Update-Metadata Repo
 cd $METADATA_DIR
 git reset --hard origin/master
+git clean -f
 git pull https://$GITHUB_TOKEN:x-oauth-basic@$METADATA_GIT master
 
 # Update _redirects
-cat << END > $METADATA_DIR/automated-deploy/_redirects
-/fdroid-repo/ChromePublic.apk $CHROME_URL 200!
-/fdroid-repo/SystemWebView.apk $WEBVIEW_URL 200!
-END
+grep -q '^\/fdroid-repo\/ChromePublic.apk' $METADATA_DIR/automated-deploy/_redirects \
+    && sed -i '/\/fdroid-repo\/ChromePublic.apk/c\\/fdroid-repo\/ChromePublic.apk '$CHROME_URL' 200!' $METADATA_DIR/automated-deploy/_redirects \
+    || echo "/fdroid-repo/ChromePublic.apk $CHROME_URL 200!" >> $METADATA_DIR/automated-deploy/_redirects
+    
+grep -q '^\/fdroid-repo\/SystemWebView.apk' $METADATA_DIR/automated-deploy/_redirects \
+    && sed -i '/\/fdroid-repo\/SystemWebView.apk/c\\/fdroid-repo\/SystemWebView.apk '$WEBVIEW_URL' 200!' $METADATA_DIR/automated-deploy/_redirects \
+    || echo "/fdroid-repo/SystemWebView.apk $WEBVIEW_URL 200!" >> $METADATA_DIR/automated-deploy/_redirects
 
 # Update APKs and metadata
 cp $APK_OUTPUT_DIR/ChromePublic.apk $METADATA_DIR/fdroid/repo/
 cp $APK_OUTPUT_DIR/SystemWebView.apk $METADATA_DIR/fdroid/repo/
 cd $METADATA_DIR/fdroid
-fdroid update || git reset --hard origin/master; exit 1
+fdroid update -c || (git reset --hard origin/master; exit 1)
 cd $METADATA_DIR/
 git add -A
 git commit -m "Update metadata and redirects for Chromium $LATEST_VERSION"
 git push https://$GITHUB_TOKEN:x-oauth-basic@$METADATA_GIT master
-
